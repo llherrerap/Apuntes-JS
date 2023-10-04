ee6d9ae2e71b459a42198b8e901e7f6f42117483
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-10-03 20:56:40 -0500

    Ejercicio calculadora

--- DOM/app.js
+++ DOM/app.js
@@ -22,5 +22,23 @@ botonFormulario.addEventListener("click", function () {
     let nombreFormulario = document.getElementById("nombre")
     //value permite obtener el valor que ingresa en una entrada de un formulario
     let parrafoRestulado = document.getElementById("resultadoFormulario")
-    parrafoRestulado.textContent=nombreFormulario.value
+    parrafoRestulado.textContent=`El nombre es: ${nombreFormulario.value}`
+})
+
+//Calculadora con el DOM
+
+//Se crea una variable resultado para guardar los resultados de las operaciones que realicemos
+let resultado
+//Obtengo por medio del DOM y del ID el elemento botonSumar
+let botonSumar=document.getElementById("sumar")
+
+//Cuando se le da click al botonSumar etonces el ejecuta la siguiente funcion
+botonSumar.addEventListener("click", function () {
+    //Obtener los numeros del formulario por medio del id, pero como el getElement obtiene TODO el elemento (hasta el HTML) y nosotros solo necesitamos el valor, utilizamos la propiedad value para extarer el valor de la entrada
+    let numeroUno = document.getElementById("num1").value
+    let numeroDos = document.getElementById("num2").value
+    //Una vez tengo los datos, realizo la operación suma, pero como el valor de los formularios esta en tipo string, se pasa a tipo entero con el metodo parseInt()
+    resultado=parseInt(numeroUno)+parseInt(numeroDos)
+    let impresionResultado = document.getElementById("resultadoCalculadora")
+    impresionResultado.textContent= `El resultado de la suma es: ${resultado}`
 })
