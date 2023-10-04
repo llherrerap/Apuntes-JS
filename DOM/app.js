ccea15d63b403741510a24529b4dab479cf1d127
Liliana Herrera <122304941+llherrerap@users.noreply.github.com>
2023-10-03 19:39:58 -0500

    Selectores DOM

---
+++ DOM/app.js
@@ -0,0 +1,6 @@
+//Como obtener un elemento en documento HTML por medio de los selectores
+
+//document hace referencia al documento HTML
+console.log(document.getElementsByTagName("h2")) //Obtener algun elemento HTML por medio de la etiqueta
+console.log(document.getElementsByClassName("elemento-selector")) //Obtener algun elemento HTML por medio de las clases
+console.log(document.getElementById("principal-title"))
